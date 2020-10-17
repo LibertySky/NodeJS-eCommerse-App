0e4185590518d8d5f14e2015d19f22dc6ea8bf3d
LibertySky <hello@LibertySkygraphics.com>
2020-10-17 16:54:13 -0400

    restructured controllers. edited vievs

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,27 +1,29 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-  res.render('admin/add-product', {
-    pageTitle: 'Add Product',
-    path: '/admin/add-product',
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true
-  });
+	res.render('admin/add-product', {
+		pageTitle: 'Add Product',
+		path: '/admin/add-product',
+	});
 };
 
 exports.postAddProduct = (req, res, next) => {
-  const product = new Product(req.body.title);
-  product.save();
-  res.redirect('/');
+	const title = req.body.title;
+	const imgUrl = req.body.imgUrl;
+	const description = req.body.description;
+	const price = req.body.price;
+
+	const product = new Product(title, imgUrl, description, price);
+	product.save();
+	res.redirect('/');
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('admin/products', {
-      prods: products,
-      pageTitle: 'Admin Products',
-      path: '/admin/products'
-    });
-  });
+	Product.fetchAll((products) => {
+		res.render('admin/products', {
+			prods: products,
+			pageTitle: 'Admin Products',
+			path: '/admin/products',
+		});
+	});
 };
