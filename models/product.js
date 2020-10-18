f7a5028414fddfd58abe7543da64f73aa8d472f6
LibertySky <hello@LibertySkygraphics.com>
2020-10-18 08:41:42 -0400

    added dynamic routes and single product details

--- models/product.js
+++ models/product.js
@@ -26,6 +26,7 @@ module.exports = class Product {
 	}
 
 	save() {
+		this.id = Math.pow(Math.random() * 99 + 1);
 		getProductsFromFile((products) => {
 			products.push(this);
 			fs.writeFile(p, JSON.stringify(products), (err) => {
@@ -36,5 +37,12 @@ module.exports = class Product {
 
 	static fetchAll(cb) {
 		getProductsFromFile(cb);
+	}
+
+	static findById(id, cb) {
+		getProductsFromFile((products) => {
+			const product = products.find((p) => p.id === id);
+			cb(product);
+		});
 	}
 };
