ce9e1c8bd06dd48f1470e2ab80f411e9da25937b
LibertySky <hello@LibertySkygraphics.com>
2020-11-05 11:03:15 -0400

    User model for db with relation to products

--- app.js
+++ app.js
@@ -7,7 +7,10 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 
+// MySQL DB handling
 const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
 // const db = require('./util/database');
 
 const app = express();
@@ -26,13 +29,17 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+// Define db tables relations
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+User.hasMany(Product);
+
+// DB synchronization
 sequelize
-	.sync()
+	.sync({ force: true }) //force to override tables in the db
 	.then((result) => {
 		// console.log(result);
 	})
 	.catch((err) => console.log(err));
 
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
