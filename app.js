07692ad32bb75b48e6db17ab173a8c2afcab6d4c
LibertySky <hello@LibertySkygraphics.com>
2020-11-05 14:14:41 -0400

    Creating & Managing a Dummy User

--- app.js
+++ app.js
@@ -24,10 +24,21 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+	User.findByPk(1)
+		.then((user) => {
+			req.user = user;
+			next();
+		})
+		.catch((err) => console.log(err));
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
+
+const PORT = process.env.PORT || 3000;
 
 // Define db tables relations
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
@@ -35,11 +46,22 @@ User.hasMany(Product);
 
 // DB synchronization
 sequelize
-	.sync({ force: true }) //force to override tables in the db
+	// .sync({ force: true }) //force to override tables in the db
+	.sync()
 	.then((result) => {
+		return User.findByPk(1);
 		// console.log(result);
 	})
-	.catch((err) => console.log(err));
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+	.then((user) => {
+		if (!user) {
+			return User.create({ name: 'LibertySky', email: 'test@test.com' });
+		}
+		return user;
+	})
+	.then((user) => {
+		// console.log(user);
+		app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+	})
+	.catch((err) => {
+		console.log(err);
+	});
