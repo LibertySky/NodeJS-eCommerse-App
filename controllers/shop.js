d0d962fb25aa922e8072ea909bb38c531ab29a9a
LibertySky <hello@LibertySkygraphics.com>
2020-11-06 09:34:00 -0400

    Orders logic & render

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,6 +1,4 @@
-const Sequelize = require('sequelize');
 const Product = require('../models/product');
-const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
 	Product.findAll()
@@ -162,11 +160,44 @@ exports.postCartDeleteProduct = (req, res, next) => {
 	// });
 };
 
+exports.postOrder = (req, res, next) => {
+	let fetchedCart;
+	req.user
+		.getCart()
+		.then((cart) => {
+			fetchedCart = cart;
+			return cart.getProducts();
+		})
+		.then((products) => {
+			return req.user.createOrder().then((order) => {
+				order.addProducts(
+					products.map((product) => {
+						product.orderItem = { quantity: product.cartItem.quantity };
+						return product;
+					})
+				);
+			});
+		})
+		.then(() => {
+			return fetchedCart.setProducts(null);
+		})
+		.then(() => {
+			res.redirect('/orders');
+		})
+		.catch((err) => console.log(err));
+};
+
 exports.getOrders = (req, res, next) => {
-	res.render('shop/orders', {
-		path: '/orders',
-		pageTitle: 'Your Orders',
-	});
+	req.user
+		.getOrders({ include: ['products'] })
+		.then((orders) => {
+			res.render('shop/orders', {
+				path: '/orders',
+				pageTitle: 'Your Orders',
+				orders: orders,
+			});
+		})
+		.catch((err) => console.log(err));
 };
 
 exports.getCheckout = (req, res, next) => {
