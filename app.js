4ccd609fe7c2dd6d224cadf8a884836804075ccb
LibertySky <hello@LibertySkygraphics.com>
2020-11-26 15:53:02 -0400

    add to cart logic

--- app.js
+++ app.js
@@ -21,9 +21,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-	User.findById('5faa9bcc70ab2c46ef9f5db0')
+	User.findById('5fad8963d36e5faa5d71827c')
 		.then((user) => {
-			req.user = user;
+			req.user = new User(user.name, user.email, user.cart, user._id);
 			next();
 		})
 		.catch((err) => console.log(err));
--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,4 @@
 const Product = require('../models/product');
-const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
 	Product.fetchAll()
@@ -65,10 +64,47 @@ exports.getCart = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
 	const prodId = req.body.productId;
-	Product.findById(prodId, (product) => {
-		Cart.addProduct(prodId, product.price);
-	});
-	res.redirect('/cart');
+	Product.findById(prodId)
+		.then((product) => {
+			return req.user.addToCart(product);
+		})
+		.then((result) => {
+			// console.log(`Shop controllers post cart result: ${result}`);
+			res.redirect('/cart');
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+	// let fetchedCart;
+	// let newQuantity = 1;
+	// req.user
+	//   .getCart()
+	//   .then(cart => {
+	//     fetchedCart = cart;
+	//     return cart.getProducts({ where: { id: prodId } });
+	//   })
+	//   .then(products => {
+	//     let product;
+	//     if (products.length > 0) {
+	//       product = products[0];
+	//     }
+
+	//     if (product) {
+	//       const oldQuantity = product.cartItem.quantity;
+	//       newQuantity = oldQuantity + 1;
+	//       return product;
+	//     }
+	//     return Product.findById(prodId);
+	//   })
+	//   .then(product => {
+	//     return fetchedCart.addProduct(product, {
+	//       through: { quantity: newQuantity }
+	//     });
+	//   })
+	//   .then(() => {
+	//     res.redirect('/cart');
+	//   })
+	//   .catch(err => console.log(err));
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
