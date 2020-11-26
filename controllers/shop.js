d3a3f700a04c904e91f7026a29b0a2dd87719ba1
LibertySky <hello@LibertySkygraphics.com>
2020-11-26 16:37:00 -0400

    Delete cart item

--- controllers/shop.js
+++ controllers/shop.js
@@ -118,10 +118,19 @@ exports.postCart = (req, res, next) => {
 
 exports.postCartDeleteProduct = (req, res, next) => {
 	const prodId = req.body.productId;
-	Product.findById(prodId, (product) => {
-		Cart.deleteProduct(prodId, product.price);
-		res.redirect('/cart');
-	});
+	req.user
+		.deleteItemFromCart(prodId)
+		.then(() => {
+			res.redirect('/cart');
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+
+	// Product.findById(prodId, (product) => {
+	// 	Cart.deleteProduct(prodId, product.price);
+	// 	res.redirect('/cart');
+	// });
 };
 
 exports.getOrders = (req, res, next) => {
