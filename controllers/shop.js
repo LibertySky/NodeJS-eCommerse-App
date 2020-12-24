86379a45ba0aac49d1cf89526ab4f1ee33a6a0b0
LibertySky <hello@LibertySkygraphics.com>
2020-12-24 16:15:31 -0400

    Pagination

--- controllers/shop.js
+++ controllers/shop.js
@@ -4,13 +4,34 @@ const Product = require('../models/product');
 const Order = require('../models/order');
 const PDFDocument = require('pdfkit');
 
+// for pagination
+const ITEMS_PER_PAGE = 1;
+const ITEMS_PER_PAGE_P = 2;
+
 exports.getProducts = (req, res, next) => {
+	// pagination
+	const page = +req.query.page || 1;
+	let totalItems;
+
 	Product.find()
+		.countDocuments()
+		.then((numProducts) => {
+			totalItems = numProducts;
+			return Product.find()
+				.skip((page - 1) * ITEMS_PER_PAGE_P)
+				.limit(ITEMS_PER_PAGE_P);
+		})
 		.then((products) => {
 			res.render('shop/product-list', {
 				prods: products,
 				pageTitle: 'All Products',
 				path: '/products',
+				currentPage: page,
+				hasNextPage: ITEMS_PER_PAGE_P * page < totalItems,
+				hasPreviousPage: page > 1,
+				nextPage: page + 1,
+				previousPage: page - 1,
+				lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE_P),
 			});
 		})
 		.catch((err) => {
@@ -38,12 +59,29 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
+	// pagination
+	const page = +req.query.page || 1;
+	let totalItems;
+
 	Product.find()
+		.countDocuments()
+		.then((numProducts) => {
+			totalItems = numProducts;
+			return Product.find()
+				.skip((page - 1) * ITEMS_PER_PAGE)
+				.limit(ITEMS_PER_PAGE);
+		})
 		.then((products) => {
 			res.render('shop/index', {
 				prods: products,
 				pageTitle: 'All Products',
 				path: '/',
+				currentPage: page,
+				hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+				hasPreviousPage: page > 1,
+				nextPage: page + 1,
+				previousPage: page - 1,
+				lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE),
 			});
 		})
 		.catch((err) => {
