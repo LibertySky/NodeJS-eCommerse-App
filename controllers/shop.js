73737b79f16cf2156e7691fe9361b13bceae6849
LibertySky <hello@LibertySkygraphics.com>
2020-12-25 11:38:27 -0400

    Payments integration with Stripe API

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,8 +1,13 @@
+require('dotenv').config();
 const fs = require('fs');
 const path = require('path');
 const Product = require('../models/product');
 const Order = require('../models/order');
 const PDFDocument = require('pdfkit');
+
+const stripe = require('stripe')(process.env.STRIPE_KEY);
+// const Stripe = require('stripe');
+// const stripe = Stripe(process.env.STRIPE_KEY);
 
 // for pagination
 const ITEMS_PER_PAGE = 1;
@@ -139,7 +144,82 @@ exports.postCartDeleteProduct = (req, res, next) => {
 		});
 };
 
+exports.getCheckout = (req, res, next) => {
+	let products;
+	let total = 0;
+	req.user
+		.populate('cart.items.productId')
+		.execPopulate()
+		.then((user) => {
+			products = user.cart.items;
+			products.forEach((p) => {
+				total += p.quantity * p.productId.price;
+			});
+
+			return stripe.checkout.sessions.create({
+				payment_method_types: ['card'],
+				line_items: products.map((p) => {
+					return {
+						name: p.productId.title,
+						description: p.productId.description,
+						amount: p.productId.price * 100,
+						currency: 'usd',
+						quantity: p.quantity,
+					};
+				}),
+				success_url:
+					req.protocol + '://' + req.get('host') + '/checkout/success',
+				cancel_url: req.protocol + '://' + req.get('host') + '/checkout/cancel',
+			});
+		})
+		.then((session) => {
+			res.render('shop/checkout', {
+				path: '/checkout',
+				pageTitle: 'Checkout',
+				products: products,
+				totalSum: total,
+				sessionId: session.id,
+			});
+		})
+		.catch((err) => {
+			const error = new Error(err);
+			error.httpStatusCode = 500;
+			return next(error);
+		});
+};
+
 exports.postOrder = (req, res, next) => {
+	req.user
+		.populate('cart.items.productId')
+		.execPopulate()
+		.then((user) => {
+			const products = user.cart.items.map((i) => {
+				return { quantity: i.quantity, product: { ...i.productId._doc } };
+			});
+			const order = new Order({
+				user: {
+					email: req.user.email,
+					userId: req.user,
+				},
+				products: products,
+			});
+			return order.save();
+			console.log(order);
+		})
+		.then(() => {
+			return req.user.clearCart();
+		})
+		.then(() => {
+			res.redirect('/orders');
+		})
+		.catch((err) => {
+			const error = new Error(err);
+			error.httpStatusCode = 500;
+			return next(error);
+		});
+};
+
+exports.getCheckoutSuccess = (req, res, next) => {
 	req.user
 		.populate('cart.items.productId')
 		.execPopulate()
@@ -184,13 +264,6 @@ exports.getOrders = (req, res, next) => {
 			error.httpStatusCode = 500;
 			return next(error);
 		});
-};
-
-exports.getCheckout = (req, res, next) => {
-	res.render('shop/checkout', {
-		path: '/checkout',
-		pageTitle: 'Checkout',
-	});
 };
 
 exports.getInvoice = (req, res, next) => {
